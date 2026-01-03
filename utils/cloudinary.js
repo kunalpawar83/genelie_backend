9e8dce993ca90d8285ee1395d3fc45c6ec63609e
kunalpawar83 <kunalpawar8319@gmaii.com>
2026-01-03 10:20:14 -0800

    checking server js file

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,28 +2,28 @@ const cloudinary = require("cloudinary").v2;
 const dotenv = require("dotenv");
 dotenv.config();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SERCRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.CLOUD_API_KEY,
+//   api_secret: process.env.CLOUD_API_SERCRET,
+// });
 
-const uploadCloudinary = (fileBuffer) => {
-  return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      { resource_type: "auto", folder: "doctors" },
-      (error, result) => {
-        if (error) {
-          console.log("Cloudinary Error:", error.message);
-          reject(error);
-        } else {
-          console.log("Upload Success:", result.secure_url);
-          resolve(result);
-        }
-      }
-    );
-    stream.end(fileBuffer);
-  });
-};
+// const uploadCloudinary = (fileBuffer) => {
+//   return new Promise((resolve, reject) => {
+//     const stream = cloudinary.uploader.upload_stream(
+//       { resource_type: "auto", folder: "doctors" },
+//       (error, result) => {
+//         if (error) {
+//           console.log("Cloudinary Error:", error.message);
+//           reject(error);
+//         } else {
+//           console.log("Upload Success:", result.secure_url);
+//           resolve(result);
+//         }
+//       }
+//     );
+//     stream.end(fileBuffer);
+//   });
+// };
 
 module.exports = { uploadCloudinary };
